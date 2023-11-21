1ad5c1ebbc681dc94210479366572b74377d3212
alexandr cumarav <alexandr.cumarav@broadcom.com>
2023-11-21 14:09:59 +0100

    working python with pgvector

--- intro-to-vector-db/pgvector-sqlmodel.py
+++ intro-to-vector-db/pgvector-sqlmodel.py
@@ -45,40 +45,37 @@ def print_documents():
 
 def write_documents():
     ai_api_key = os.environ["OPENAI_API_KEY"]
-    client = OpenAI(api_key=ai_api_key, base_url="https://api.openai.com/v1/embeddings")
-    # openssl s_client -showcerts -verify 5 -connect https://api.openai.com/v1/embeddings:443 < /dev/null
-    # check requests to OpenAI
+    client = OpenAI(api_key=ai_api_key)
     print(f"\napi key={ai_api_key}")
 
     loader = DirectoryLoader(
-        "./apiml_docs", glob="**/*12).md", loader_cls=UnstructuredMarkdownLoader
+        "./apiml_docs", glob="**/*.md", loader_cls=UnstructuredMarkdownLoader
     )
     my_documents = loader.load()
     print(f"Found {len(my_documents)} documents")
     text_splitter = MarkdownTextSplitter(chunk_size=2000, chunk_overlap=200)
     texts = text_splitter.split_documents(my_documents)
     print(f"Produced {len(texts)} chunks to embed")
-    unsaved_doc = embed_one_document(texts[0], client=client)
-    write_document(unsaved_doc)
+    for doc in texts:
+        unsaved_doc = embed_one_document(doc, client=client)
+        write_document(unsaved_doc)
 
 
-def embed_one_document(text: LangDocument, client: OpenAI) -> PgDocument:
+def embed_one_document(lang_doc: LangDocument, client: OpenAI) -> PgDocument:
     new_document = PgDocument()
-    new_document.file_path = text.metadata["source"]
-    new_document.content = text.page_content
-    res = client.embeddings.create(input=[text.page_content], model=EMBEDDINGS_MODEL )
-    print(f'Generated embeddings for the string "{text[0:100]}", dimensions: {len(res["data"][0]["embedding"])}')
-    vector = res["data"][0]["embedding"]
+    new_document.file_path = lang_doc.metadata["source"]
+    new_document.content = lang_doc.page_content
+    res = client.embeddings.create(input=[lang_doc.page_content], model=EMBEDDINGS_MODEL)
+    print(f'Generated embeddings for the string "{lang_doc.page_content[0:100]}", dimensions: {len(res.data[0].embedding)}')
+    vector = res.data[0].embedding
     new_document.embedded = vector
     print(f"New doc: {new_document}")
     return new_document
 
 
 def main():
-    #os.environ['REQUESTS_CA_BUNDLE']='c:\\keys\\cloud_services_root_CA.crt'
     print(f"Connection string: {CONNECTION_STRING}")
     init_db()
-    # print_documents()
     write_documents()
 
 
